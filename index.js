ff46262b106c273076ac90d605e092dbf3463577
ralph1786 <ralph1786@gmail.com>
2019-01-28 17:25:08 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+    let begin = parseInt(start);
+    let finish = parseInt(end);
+    let distanceTravelled = Math.abs(finish - begin);
+    let difference = blockRange - distanceTravelled;
+
+    if (difference > 0) {
+      return `within range by ${difference}`;
+    } else {
+      return `${Math.abs(difference)} blocks out of range`;
+    }
+  };
+}
+
+function produceTipCalculator(percent) {
+  return function(fare) {
+    return fare * percent;
+  };
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.id = ++driverId;
+    }
+  };
+}
